cb8f76c66832d3a92797be873c5f3077e2731922
Harmeet singh <147964541+Harmeetsingh0201@users.noreply.github.com>
2023-11-26 18:38:22 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number:";
+    cin >> num1;
+    cout << "Enter operator";
+    cin >> op;
+    cout << "Enter Second Number:" ;
+    cin >> num2;
+    int result;
+
+    if (op == '+')
+    {
+        result= num1 + num2;
+    }
+    else if (op == '-')
+    {
+        result= num1 - num2;
+    }
+    else if (op=='*')
+    {
+        result= num1*num2;
+    }
+    else if (op== '/')
+    {
+        result= num1/num2;
+    }
+    else
+    {
+        cout << "invalid operator!";
+    }
+    cout << result;
+    return 0;
+}
